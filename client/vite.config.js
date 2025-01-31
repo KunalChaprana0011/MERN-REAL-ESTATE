408ee8250f5c6cf943da23cc90ab88bf21ea56b1
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-01-31 08:12:03 +0530

    complete signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
