0130a848e9db72d674d4b01889609d0cb4fde733
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-03-10 21:44:14 +0530

    complete search image api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing,deleteuserLisitng,updateuserLisitng,getLisitng } from '../controllers/listing.controller.js'
+import { createListing,deleteuserLisitng,updateuserLisitng,getLisitng ,getUserListings} from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteuserLisitng)
 router.post('/update/:id',verifyToken,updateuserLisitng)
 router.get('/get/:id',getLisitng)
+router.get('/get', getUserListings);
 
 export default router;
